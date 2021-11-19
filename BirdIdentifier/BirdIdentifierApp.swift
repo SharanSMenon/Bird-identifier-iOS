f37013208071f7fb228cb70f218a787e3d031587
sharansajivmenon <sharansajivmenon@gmail.com>
2021-11-18 19:53:55 -0500

    Initial Commit

---
+++ BirdIdentifier/BirdIdentifierApp.swift
@@ -0,0 +1,20 @@
+//
+//  BirdIdentifierApp.swift
+//  BirdIdentifier
+//
+//  Created by Sharan Sajiv Menon on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct BirdIdentifierApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
